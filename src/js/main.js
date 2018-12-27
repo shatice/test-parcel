b5975f100a239f8bdf928fc854109f995a955fbc
SHatice <sarah.gungor@hetic.net>
2018-12-28 00:44:49 +0100

    add header fixed & burger menu, hide old js tests, organise styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
+var menu = document.getElementById('menu');
+var bgmenu = document.getElementById('menu__burger');
 
+bgmenu.addEventListener('click', function() {
+  menu.classList.toggle('is-open');
+});
